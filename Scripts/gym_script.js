4d621aecd919db659dfeafc80894c0769c3c90ae
Yu-ChengDutch <zmezl1423@gmail.com>
2026-01-03 19:41:32 +0100

    Improved randomisation

--- Scripts/gym_script.js
+++ Scripts/gym_script.js
@@ -1,3 +1,7 @@
+// Constants
+const rand_array = new Uint8Array(50);
+self.crypto.getRandomValues(rand_array);
+
 // Function library
 
 function toggle_song(section) {
@@ -38,7 +42,7 @@ function up_counter(el) {
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor((rand_array[i] / 255) * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
